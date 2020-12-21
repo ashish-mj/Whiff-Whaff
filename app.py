13b60da94d2aaa8b59df5081857521a0f0b9da91
Ashish M J <ashm.jagadeesh@gmail.com>
2020-12-21 13:59:41 +0530

    Feature : Collision

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ cpu = pygame.Rect(10,screen_height/2-70,10,140)
 background = pygame.Color('grey12')
 border = (200,200,200)
 
+ball_speed_x,ball_speed_y = 6,6
 
 while True:
     
@@ -33,6 +34,15 @@ while True:
             pygame.quit()
             sys.exit()
     
+    ball.x+=ball_speed_x
+    ball.y+=ball_speed_y
+    
+    if ball.top <=0 or ball.bottom>= screen_height:
+        ball_speed_y *= -1
+    if ball.left <=0 or ball.right>= screen_width:
+        ball_speed_x *= -1
+    if ball.colliderect(player) or ball.colliderect(cpu):
+        ball_speed_x *= -1
     
     screen.fill(background)
     pygame.draw.rect(screen,border,player)
