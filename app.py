907ed7d1e66cb6a18216eca01257181cc15994b6
Ashish M J <ashm.jagadeesh@gmail.com>
2020-12-22 11:10:10 +0530

    Timer Update

--- app.py
+++ app.py
@@ -10,19 +10,22 @@ import pygame,sys,random
 
 def ball_movement():
     
-    global ball_speed_x,ball_speed_y,player_score,cpu_score
+    global ball_speed_x,ball_speed_y,player_score,cpu_score,score_time
     
     ball.x+=ball_speed_x
     ball.y+=ball_speed_y
     
     if ball.top <=0 or ball.bottom>= screen_height:
         ball_speed_y *= -1
+        
     if ball.left <=0:
         player_score += 1
-        ball_restart()
+        score_time = pygame.time.get_ticks()
+        
     if ball.right>= screen_width:
         cpu_score += 1
-        ball_restart()
+        score_time = pygame.time.get_ticks()
+        
     if ball.colliderect(player) or ball.colliderect(cpu):
         ball_speed_x *= -1
  
@@ -44,10 +47,29 @@ def cpu_movement():
         cpu.bottom =screen_height
         
 def ball_restart():
-    global ball_speed_x,ball_speed_y
+    global ball_speed_x,ball_speed_y, score_time
+    
+    current_time = pygame.time.get_ticks()
     ball.center = (screen_width/2,screen_height/2)
-    ball_speed_y *= random.choice((1,-1))
-    ball_speed_x *= random.choice((1,-1))
+    
+    if current_time - score_time < 700:
+        three = game_font.render("3",False, border)
+        screen.blit(three,(screen_width/2-10,screen_height/2+20))
+        
+    if 700 < current_time - score_time < 1400:
+        two = game_font.render("2",False, border)
+        screen.blit(two,(screen_width/2-10,screen_height/2+20))
+        
+    if 1400 < current_time - score_time < 2100:
+        one = game_font.render("1",False, border)
+        screen.blit(one,(screen_width/2-10,screen_height/2+20))
+        
+    if current_time - score_time < 2100:
+        ball_speed_x,ball_speed_y = 0,0
+    else:
+        ball_speed_y = 7* random.choice((1,-1))
+        ball_speed_x = 7* random.choice((1,-1))
+        score_time = None
 
 
 pygame.init()
@@ -69,12 +91,14 @@ border = (200,200,200)
 
 ball_speed_x = 6 * random.choice((1,-1))
 ball_speed_y = 6 * random.choice((1,-1))
-player_speed, cpu_speed = 0,6
+player_speed, cpu_speed = 0,7
 
 
 player_score = 0
 cpu_score = 0
 game_font = pygame.font.Font("freesansbold.ttf",32)
+
+score_time = True
 
 while True:
     
@@ -105,11 +129,14 @@ while True:
     pygame.draw.ellipse(screen,border,ball)
     pygame.draw.aaline(screen,border,(screen_width/2,0),(screen_width/2,screen_height))
     
+    if score_time:
+        ball_restart()
+    
     player_text = game_font.render(f"{player_score}",False,border)
-    screen.blit(player_text,(660,300))
+    screen.blit(player_text,(660,290))
     
     cpu_text = game_font.render(f"{cpu_score}",False,border)
-    screen.blit(cpu_text,(583,300))
+    screen.blit(cpu_text,(583,290))
     
     pygame.display.flip()
     clock.tick(60)
