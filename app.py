b891e450cadb43ab084ddb95d572b12e81208180
Ashish M J <ashm.jagadeesh@gmail.com>
2020-12-22 10:06:27 +0530

    Score Update

--- app.py
+++ app.py
@@ -8,39 +8,20 @@ Created on Mon Dec 21 10:15:24 2020
 
 import pygame,sys,random
 
-
-
-pygame.init()
-clock = pygame.time.Clock()
-
-
-screen_width = 1260
-screen_height = 600
-
-screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.display.set_caption('Ping Pong')
-
-ball = pygame.Rect(screen_width/2-15,screen_height/2-15,30,30)
-player = pygame.Rect(screen_width-20,screen_height/2-70,10,140)
-cpu = pygame.Rect(10,screen_height/2-70,10,140)
-
-background = pygame.Color('grey12')
-border = (200,200,200)
-
-ball_speed_x = 6 * random.choice((1,-1))
-ball_speed_y = 6 * random.choice((1,-1))
-player_speed, cpu_speed = 0,6
-
 def ball_movement():
     
-    global ball_speed_x,ball_speed_y
+    global ball_speed_x,ball_speed_y,player_score,cpu_score
     
     ball.x+=ball_speed_x
     ball.y+=ball_speed_y
     
     if ball.top <=0 or ball.bottom>= screen_height:
         ball_speed_y *= -1
-    if ball.left <=0 or ball.right>= screen_width:
+    if ball.left <=0:
+        player_score += 1
+        ball_restart()
+    if ball.right>= screen_width:
+        cpu_score += 1
         ball_restart()
     if ball.colliderect(player) or ball.colliderect(cpu):
         ball_speed_x *= -1
@@ -68,6 +49,33 @@ def ball_restart():
     ball_speed_y *= random.choice((1,-1))
     ball_speed_x *= random.choice((1,-1))
 
+
+pygame.init()
+clock = pygame.time.Clock()
+
+
+screen_width = 1260
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Ping Pong')
+
+ball = pygame.Rect(screen_width/2-15,screen_height/2-15,30,30)
+player = pygame.Rect(screen_width-20,screen_height/2-70,10,140)
+cpu = pygame.Rect(10,screen_height/2-70,10,140)
+
+background = pygame.Color('grey12')
+border = (200,200,200)
+
+ball_speed_x = 6 * random.choice((1,-1))
+ball_speed_y = 6 * random.choice((1,-1))
+player_speed, cpu_speed = 0,6
+
+
+player_score = 0
+cpu_score = 0
+game_font = pygame.font.Font("freesansbold.ttf",32)
+
 while True:
     
     for event in pygame.event.get():
@@ -76,10 +84,16 @@ while True:
             sys.exit()
         
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                player_speed += 7
             if event.key == pygame.K_UP:
-                player_speed -= 7
+                player_speed -= 6
+            if event.key == pygame.K_DOWN:
+                player_speed += 6
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                player_speed += 6
+            if event.key == pygame.K_DOWN:
+                player_speed -= 6
     
     ball_movement()
     player_movement()
@@ -90,6 +104,13 @@ while True:
     pygame.draw.rect(screen,border,cpu)
     pygame.draw.ellipse(screen,border,ball)
     pygame.draw.aaline(screen,border,(screen_width/2,0),(screen_width/2,screen_height))
+    
+    player_text = game_font.render(f"{player_score}",False,border)
+    screen.blit(player_text,(660,300))
+    
+    cpu_text = game_font.render(f"{cpu_score}",False,border)
+    screen.blit(cpu_text,(583,300))
+    
     pygame.display.flip()
     clock.tick(60)
     
