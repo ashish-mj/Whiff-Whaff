779669b9d8680bb71ed699602e403056848d75e2
Ashish M J <73954439+ashish-mj@users.noreply.github.com>
2020-12-23 07:32:43 +0530

    Update title

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ screen_width = 1260
 screen_height = 600
 
 screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.display.set_caption('Ping Pong')
+pygame.display.set_caption('Whiff Whaff')
 
 ball = pygame.Rect(screen_width/2-15,screen_height/2-15,30,30)
 player = pygame.Rect(screen_width-20,screen_height/2-70,10,140)
